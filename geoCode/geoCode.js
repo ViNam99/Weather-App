6f3c32ad9f60b86b5a9f2f0557e22ffcd7deb387
Nhan Vi Nam <nhanvinamcoder@gmail.com>
2020-09-07 16:45:46 +0700

    exports module and additional callBack

---
+++ geoCode/geoCode.js
@@ -0,0 +1,33 @@
+const request = require("request");
+
+const geoCodeAddress = (address, callBack) => {
+  let encodedAddress = encodeURIComponent(address);
+
+  request.get(
+    {
+      url: `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyDBunJ4GXNEC3KJlpoGJO-iB--CjPv4o-s&address=${encodedAddress}`,
+      json: true,
+    },
+    (err, response, body) => {
+      if (err && err.code === "ENOTFOUND")
+        return callBack(undefined, "Cannot Connect to maps.googleapis.com");
+      // return console.log("Cannot Connect to maps.googleapis.com");
+      if (body.status === "ZERO_RESULTS")
+        return callBack(undefined, "Address not Found");
+      // return console.log("Address not Found");
+
+      const { formatted_address, geometry } = body.results[0];
+      const { lat, lng } = geometry.location;
+
+      callBack(
+        {
+          Address: formatted_address,
+          Latitude: lat,
+          Longtitude: lng,
+        },
+        undefined
+      );
+    }
+  );
+};
+module.exports.geoCodeAddress = geoCodeAddress;
---
+++ revise.js
@@ -0,0 +1,21 @@
+const geoCode = require("./geoCode/geoCode");
+const yargs = require("yargs");
+
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Enter your Address",
+      string: true,
+    },
+  })
+  .help()
+  .alias("help", "h").argv;
+
+// console.log(argv);
+
+geoCode.geoCodeAddress(argv.address, (res, err) => {
+  if (err) return console.log(err);
+  console.log(res);
+});
